146db857df19c5b28242e7a2595d5c330d8a6b87
lato99 <lato18@ku.edu.tr>
2024-01-01 16:49:56 +0300

    python solution update

--- python/455. Assign Cookies.py
+++ python/455. Assign Cookies.py
@@ -1,18 +1,31 @@
 from ast import List
 
-
 class Solution:
     def findContentChildren(self, g: List[int], s: List[int]) -> int:
         gSorted, sSorted = sorted(g), sorted(s)
-        l,r = 0, 0
-        noContentChildren = 0
-        while(l < len(gSorted) and r < len(sSorted)):
-            gSize = gSorted[l]
+        noContentChildren,r = 0, 0
+        while(noContentChildren < len(gSorted) and r < len(sSorted)):
+            gSize = gSorted[noContentChildren]
             sSize = sSorted[r]
             if(gSize <= sSize):
-                l+=1
-                r+=1
                 noContentChildren+=1
+                r+=1
             else:
                 r += 1
         return noContentChildren
+
+# class Solution:
+#     def findContentChildren(self, g: List[int], s: List[int]) -> int:
+#         gSorted, sSorted = sorted(g), sorted(s)
+#         l,r = 0, 0
+#         noContentChildren = 0
+#         while(l < len(gSorted) and r < len(sSorted)):
+#             gSize = gSorted[l]
+#             sSize = sSorted[r]
+#             if(gSize <= sSize):
+#                 l+=1
+#                 r+=1
+#                 noContentChildren+=1
+#             else:
+#                 r += 1
+#         return noContentChildren
