94182a0ed53885779abe119ef2712f87093349dd
lato99 <lato18@ku.edu.tr>
2024-01-23 22:07:07 +0300

    197. Rising Temperature sql solution

---
+++ sql/197. Rising Temperature.sql
@@ -0,0 +1,7 @@
+-- Write your MySQL query statement below
+select w2.id from Weather w1, Weather w2
+where datediff(w2.recordDate,w1.recordDate) = 1 and w2.temperature > w1.temperature;
+
+-- Write your MySQL query statement below
+select w2.id from Weather w2
+join Weather w1 on datediff(w2.recordDate,w1.recordDate) = 1 and w2.temperature > w1.temperature;
