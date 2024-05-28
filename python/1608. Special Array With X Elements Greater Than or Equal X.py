12ad62c7864f264de9a03787a27404a534399012
lato18@ku.edu.tr <lato18@ku.edu.tr>
2024-05-28 11:40:27 +0300

    fix

--- python/1608. Special Array With X Elements Greater Than or Equal X.py
+++ python/1608. Special Array With X Elements Greater Than or Equal X.py
@@ -1,5 +1,5 @@
 # Time Complexity : O(n logn), where n is the length of nums. Sorting takes O(n logn) time. After sorting, the nums array is traversed, with constant time operations. So traversing the nums array takes O(n) time. Overall time complexity is O(n logn) time.
-# Space Complexity : O(n logn), sorting takes O(n logn) time, and there are no other data structure used except constants. 
+#  Space Complexity : O(n logn), sorting takes O(n logn) time, and there are no other data structure used except constants. 
 
 class Solution:
     def specialArray(self, nums: List[int]) -> int:
